6b455a11d730b2079ed7781770444fa4f16552b0
LoreBarrera <karenlbarrera@gmail.com>
2023-10-23 12:31:53 -0500

    Configuraciones e instalaciones

---
+++ examples/ejemplo1.md
@@ -0,0 +1,2 @@
+//archivos de ejemplo, que validan que la funcion mdlinks cumple el proposito// 
+ 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//vamos a llamar funciones- aqui viviran las funciones de mdlinks que llamara a las microfunciones de apps.js//
 module.exports = () => {
   // ...
 };
