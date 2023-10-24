7f5399960fa5df30d28923c63044c5ee41c727e2
LoreBarrera <karenlbarrera@gmail.com>
2023-10-24 18:06:37 -0500

    promesa con ruta absoluta

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
 const marked = require('marked');
 
-function analyzeLinks (markdownFilePath) {
-  // Leer el contenido del archivo Markdown
-  const markdownContent = fs.readFileSync(markdownFilePath, 'utf8');
-
-  // Utilizar marked para convertir el contenido Markdown a tokens
-  const tokens = marked.lexer(markdownContent);
-
-  // Realizar el análisis de enlaces y devolverlos
-  const links = [];
-
-  for (const token of tokens) {
-    if (token.type === 'link') {
-      const href = token.href;
-      const text = token.text;
-      links.push({ href, text });
-    }
-  }
-
-  return links;
+function readFilePromise(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
-module.exports = {
-  analyzeLinks
-};
+function analyzeLinks(filePath) {
+  return readFilePromise(filePath)
+    .then(markdownContent => {
+      const tokens = marked.lexer(markdownContent);
+      const links = [];
+
+      for (const token of tokens) {
+        if (token.type === 'link') {
+          const href = token.href;
+          const text = token.text;
+          links.push({ href, text });
+        }
+      }
+
+      return links;
+    });
+}
