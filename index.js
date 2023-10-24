9a3742d7b6cd35278535313be1c173ec92e2f760
LoreBarrera <karenlbarrera@gmail.com>
2023-10-23 20:59:57 -0500

    modulos

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-//vamos a llamar funciones- aqui viviran las funciones de mdlinks que llamara a las microfunciones de apps.js//
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const marked = require('marked');
+
+function analyzeLinks(markdownFilePath) {
+  // Leer el contenido del archivo Markdown
+  const markdownContent = fs.readFileSync(markdownFilePath, 'utf8');
+
+  // Utilizar marked para convertir el contenido Markdown a tokens
+  const tokens = marked.lexer(markdownContent);
+
+  // Realizar el análisis de enlaces y devolverlos
+  const links = [];
+
+  for (const token of tokens) {
+    if (token.type === 'link') {
+      const href = token.href;
+      const text = token.text;
+      links.push({ href, text });
+    }
+  }
+
+  return links;
+}
+
+module.exports = {
+  analyzeLinks,
 };
