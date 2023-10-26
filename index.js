366b7c0f6aa06183d3f826b4e0a0a96caad2b766
LoreBarrera <karenlbarrera@gmail.com>
2023-10-26 11:52:02 -0500

    arreglo de las exportaciones

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-const fs = require('fs');
-const marked = require('marked');
+const path = require('path');
+const analyzeLinks = require('./lib/apps');
 
-function readFilePromise(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
+const relativePath = 'examples/ejemplo1.md';
+const absolutePath = path.resolve(relativePath);
+
+analyzeLinks(absolutePath)
+  .then(links => {
+    links.forEach((link, index) => {
+      console.log(`Enlace ${index + 1}:`);
+      console.log(`- Texto: ${link.text}`);
+      console.log(`- URL: ${link.href}`);
+      console.log('');
     });
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
-}
 
-function analyzeLinks(filePath) {
-  return readFilePromise(filePath)
-    .then(markdownContent => {
-      const tokens = marked.lexer(markdownContent);
-      const links = [];
 
-      for (const token of tokens) {
-        if (token.type === 'link') {
-          const href = token.href;
-          const text = token.text;
-          links.push({ href, text });
-        }
-      }
+console.log('Función analyzeLinks:', analyzeLinks);
 
-      return links;
-    });
-}
+module.exports = analyzeLinks;
