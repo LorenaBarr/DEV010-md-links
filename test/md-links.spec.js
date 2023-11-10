e78fd703425630500444a219dc718f42377a8a23
LoreBarrera <karenlbarrera@gmail.com>
2023-11-09 22:34:38 -0500

    Estructura de test de funcion isAbsolutePath, convertToAbsolutePath, mdlinks, pero aun no pasan, al parecer el error es la exportacion

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,14 @@
+const path = require('path');
+const isAbsolutePath = require('../lib/mdLinks');
+const convertToAbsolutePath = require('../lib/mdLinks'); 
+const doesFileExist = require('../lib/mdLinks'); 
 const mdLinks = require('../lib/mdLinks'); 
+const exp = require('constants');
+const fs = require('fs').promises;
+
+//const mdLinks = require('../lib/mdLinks');
+//const { isAbsolutePath, convertToAbsolutePath } = require('../lib/mdLinks');
+const { describe, it } = require('@jest/globals');
 
 
 describe('mdLinks', () => {
@@ -19,6 +29,45 @@ describe('mdLinks', () => {
   });
 });
 
+describe('convertToAbsolutePath', () => {
+  it('Debería retornar la ruta absoluta si es relativa', () => {
+    const relativePath = 'test/pruebastest/testFile.md';
+    const absolutePath = path.resolve(relativePath);
+    expect(convertToAbsolutePath(relativePath)).toEqual(absolutePath);
+  });
+});
+
+describe('isAbsolutePath', () => {
+  it('debería retornar la misma ruta si es absoluta', () => {
+    const absolutePath = 'C:/Users/LOREN/DEV010-md-links/test/pruebastest/testFile.md';
+    expect(isAbsolutePath(absolutePath)).toEqual(absolutePath);
+  });
+});
+
+describe('doesFileExist', () => {
+  it('deberia resolver true si el archivo existe', async () => {
+    const fileExist = 'C:/Users/LOREN/DEV010-md-links/test/pruebastest/testFile.md';
+    try {
+      const result = await doesFileExist(fileExist);
+      expect(result).toBe(true);
+    } catch (error) {
+      fail('La promesa debería resolverse, pero se rechazó');
+    }
+  });
+
+  it('Deberia rechazar con un error si el archivo no existe', async () => {
+    const notFileExist = 'ruta/inexistente/al/archivo.txt';
+    try {
+      await doesFileExist(notFileExist);
+      fail('La promesa debería rechazarse');
+    } catch (error) {
+      expect(error instanceof Error).toBe(true);
+      // Aquí debes ajustar el mensaje según lo que devuelve tu función doesFileExist
+      // Esperas 'The path does not exist', pero esto depende de la implementación real de tu función
+      expect(error.message).toBe('El archivo no existe.');
+    }
+  });
+});
 
 
  
