99d2a0aedff80ff39de388c42d0b08e145d7241c
LoreBarrera <karenlbarrera@gmail.com>
2023-11-14 12:40:33 -0500

    Se agrego libreria para mockear fecth

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -5,7 +5,7 @@ const { describe, it } = require('@jest/globals');
 //test de mdlinks devuelve el array
 describe('mdLinks', () => {
   it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => {
-    return mdLinks('test/test/testFile.md').then((links) => {
+    return mdLinks('test/pruebastest').then((links) => {
       // Comprueba que el resultado sea un arreglo
       expect(Array.isArray(links)).toBe(true);
       // Comprueba que haya 3 links en el arreglo
